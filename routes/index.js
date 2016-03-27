9d2d2d9a588da5f69676e7edd9a4dea930ec6574
zfpx <zfpx@126.com>
2016-03-27 12:38:38 +0800

    添加事项和列表

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var todoModel = require('../model');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/todos', function(req, res, next) {
+  todoModel.find({},function(err,todos){
+    if(err){
+      res.send({code:0,msg:'查询错误!'});
+    }else{
+      res.send(todos);
+    }
+  });
+});
+
+router.post('/todos',function(req,res){
+  console.log(req.body);
+  todoModel.create(req.body,function(err,todo){
+    if(err){
+      console.log(err);
+      res.send({code:0,msg:'添加错误!'});
+    }else{
+      //把保存到数据库中之后的对象发送给客户端
+      res.send(todo);
+    }
+  });
 });
 
 module.exports = router;
